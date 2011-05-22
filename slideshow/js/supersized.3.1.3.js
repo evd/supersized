cfe96cf85884156e64018052dd398374d4237240
evd <toevgd@gmail.com>
2011-05-22 23:45:42 +0400

    Add fit_image option. Simplify resize code, possibly broke other resize functional

--- slideshow/js/supersized.3.1.3.js
+++ slideshow/js/supersized.3.1.3.js
@@ -42,6 +42,7 @@
 			horizontal_center       :   1,		//Horizontally center background
 			fit_portrait         	:   0,		//Portrait images will not exceed browser height
 			fit_landscape			:   0,		//Landscape images will not exceed browser width  
+			fit_image				:   0,		//All images will not exceed browser width or height
 			
 			//Components
 			navigation              :   1,		//Slideshow controls on/off
@@ -409,99 +410,28 @@
 					
 					/**Resize image to proper ratio**/
 					
-					if ((browserheight <= options.min_height) && (browserwidth <= options.min_width)){	//If window smaller than minimum width and height
-					
-						if ((browserheight/browserwidth) > ratio){
-							options.fit_landscape && ratio <= 1 ? resizeWidth(true) : resizeHeight(true);	//If landscapes are set to fit
-						} else {
-							options.fit_portrait && ratio > 1 ? resizeHeight(true) : resizeWidth(true);		//If portraits are set to fit
-						}
-					
-					} else if (browserwidth <= options.min_width){		//If window only smaller than minimum width
-					
-						if ((browserheight/browserwidth) > ratio){
-							options.fit_landscape && ratio <= 1 ? resizeWidth(true) : resizeHeight();	//If landscapes are set to fit
-						} else {
-							options.fit_portrait && ratio > 1 ? resizeHeight() : resizeWidth(true);		//If portraits are set to fit
-						}
-						
-					} else if (browserheight <= options.min_height){	//If window only smaller than minimum height
-					
-						if ((browserheight/browserwidth) > ratio){
-							options.fit_landscape && ratio <= 1 ? resizeWidth() : resizeHeight(true);	//If landscapes are set to fit
-						} else {
-							options.fit_portrait && ratio > 1 ? resizeHeight(true) : resizeWidth();		//If portraits are set to fit
-						}
-					
-					} else {	//If larger than minimums
-						
-						if ((browserheight/browserwidth) > ratio){
-							options.fit_landscape && ratio <= 1 ? resizeWidth() : resizeHeight();	//If landscapes are set to fit
-						} else {
-							options.fit_portrait && ratio > 1 ? resizeHeight() : resizeWidth();		//If portraits are set to fit
-						}
-						
+					var min_width = options.min_width;
+					var max_width = (options.fit_image || (options.fit_landscape && ratio<=1))?browserwidth:0;
+					var min_height = options.min_height;
+					var max_height = (options.fit_image || (options.fit_portrait && ratio>1))?browserheight:0;
+
+					var newwidth = browserwidth;
+					if (newwidth<options.min_width)
+						newwidth = options.min_width;
+					if (max_width>0 && newwidth>max_width)
+						newwidth = max_width;
+					var newheight = newwidth*ratio;
+					if (newheight<options.min_height) {
+						newheight = options.min_height;
+						newwidth = newheigh/ratio;
 					}
-					
-					/**End Image Resize**/
-					
-					
-					/**Resize Functions**/
-					
-					function resizeWidth(minimum){
-						if (minimum){	//If minimum height needs to be considered
-							if(thisSlide.width() < browserwidth || thisSlide.width() < options.min_width ){
-								if (thisSlide.width() * ratio >= options.min_height){
-									thisSlide.width(options.min_width);
-						    		thisSlide.height(thisSlide.width() * ratio);
-						    	}else{
-						    		resizeHeight();
-						    	}
-						    }
-						}else{
-							if (options.min_height >= browserheight && !options.fit_landscape){	//If minimum height needs to be considered
-								if (browserwidth * ratio >= options.min_height || (browserwidth * ratio >= options.min_height && ratio <= 1)){	//If resizing would push below minimum height or image is a landscape
-									thisSlide.width(browserwidth);
-									thisSlide.height(browserwidth * ratio);
-								} else if (ratio > 1){		//Else the image is portrait
-									thisSlide.height(options.min_height);
-									thisSlide.width(thisSlide.height() / ratio);
-								} else if (thisSlide.width() < browserwidth) {
-									thisSlide.width(browserwidth);
-						    		thisSlide.height(thisSlide.width() * ratio);
-								}
-							}else{	//Otherwise, resize as normal
-								thisSlide.width(browserwidth);
-								thisSlide.height(browserwidth * ratio);
-							}
-						}
-					};
-					
-					function resizeHeight(minimum){
-						if (minimum){	//If minimum height needs to be considered
-							if(thisSlide.height() < browserheight){
-								if (thisSlide.height() / ratio >= options.min_width){
-									thisSlide.height(options.min_height);
-									thisSlide.width(thisSlide.height() / ratio);
-								}else{
-									resizeWidth(true);
-								}
-							}
-						}else{	//Otherwise, resized as normal
-							if (options.min_width >= browserwidth){	//If minimum width needs to be considered
-								if (browserheight / ratio >= options.min_width || ratio > 1){	//If resizing would push below minimum width or image is a portrait
-									thisSlide.height(browserheight);
-									thisSlide.width(browserheight / ratio);
-								} else if (ratio <= 1){		//Else the image is landscape
-									thisSlide.width(options.min_width);
-						    		thisSlide.height(thisSlide.width() * ratio);
-								}
-							}else{	//Otherwise, resize as normal
-								thisSlide.height(browserheight);
-								thisSlide.width(browserheight / ratio);
-							}
-						}
-					};
+					if (max_height>0 && newheight>max_height) {
+						newheight = max_height;
+						newwidth = newheight/ratio;
+					}
+
+					thisSlide.width(newwidth);
+					thisSlide.height(newheight);
 					
 					/**End Resize Functions**/
 					
